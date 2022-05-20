122c6b4c9bb8c9bac32817f325ddea251bd205a5
Jeremiah McCarthy <jeremiahmccarthy@xmos.com>
2022-05-20 09:54:29 -0400

    Cleanup apps, examples, tests, CMake

--- examples/freertos/explorer_board/src/gpio_ctrl/gpio_ctrl.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2019-2021 XMOS LIMITED.
-// This Software is subject to the terms of the XMOS Public Licence: Version 1.
-
-#ifndef GPIO_CTRL_H_
-#define GPIO_CTRL_H_
-
-void gpio_ctrl_create( UBaseType_t priority );
-
-#endif /* GPIO_CTRL_H_ */
