a29bfa491c4ba9fcd60cb5f5b9fa6e0c2f3965b4
Jeremiah McCarthy <jeremiahmccarthy@xmos.com>
2022-05-23 16:07:42 -0400

    Remove multitile support module

--- modules/multitile_support/api/soc.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2019-2021 XMOS LIMITED.
-// This Software is subject to the terms of the XMOS Public Licence: Version 1.
-
-#ifndef SOC_H_
-#define SOC_H_
-
-#include "soc_channel.h"
-
-#endif /* SOC_H_ */
