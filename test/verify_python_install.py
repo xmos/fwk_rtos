c44e09e92fe4c64901432f43768ef67a0678e997
Jeremiah McCarthy <jeremiahmccarthy@xmos.com>
2022-05-24 11:26:19 -0400

    Remove tests at this level

--- test/verify_python_install.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2021 XMOS LIMITED.
-# This Software is subject to the terms of the XMOS Public Licence: Version 1.
-
-# importing several requirements to verify the install
-
-import numpy
-import matplotlib
-from PIL import Image
