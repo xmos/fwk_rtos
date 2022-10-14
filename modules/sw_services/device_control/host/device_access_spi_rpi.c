1c002262d22438e2fb32717ddc9c012f1db49f72
Shuchita Khare <shuchitak@xmos.com>
2022-10-14 19:35:43 +0100

    Added updated device_control transport host code

--- modules/sw_services/device_control/host/device_access_spi_rpi.c
+++ modules/sw_services/device_control/host/device_access_spi_rpi.c
@@ -1,4 +1,4 @@
-// Copyright 2017-2021 XMOS LIMITED.
+// Copyright 2017-2022 XMOS LIMITED.
 // This Software is subject to the terms of the XMOS Public Licence: Version 1.
 #if USE_SPI && RPI
 
@@ -8,6 +8,7 @@
 #include "device_control_host.h"
 #include "control_host_support.h"
 #include "bcm2835.h"
+#include <time.h>
 
 //#define DBG(x) x
 #define DBG(x)
@@ -27,30 +28,46 @@ control_init_spi_pi(spi_mode_t spi_mode, bcm2835SPIClockDivider clock_divider, u
 {
   if(!bcm2835_init() ||
      !bcm2835_spi_begin()) {
-    fprintf(stderr, "bcm2835 initialisation failed. Possibly not running as root\n");
+    fprintf(stderr, "BCM2835 initialisation failed. Possibly not running as root\n");
     return CONTROL_ERROR;
   }
 
   delay_milliseconds = delay_for_read;
 
   bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);
-  bcm2835_spi_setDataMode(lib_spi_to_bcm2835_mode(spi_mode));
+  bcm2835_spi_setDataMode(spi_mode);
   bcm2835_spi_setClockDivider(clock_divider);
   bcm2835_spi_chipSelect(BCM2835_SPI_CS0);
   bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);
 	
   return CONTROL_SUCCESS;
 }
-
 control_ret_t
 control_write_command(control_resid_t resid, control_cmd_t cmd,
                       const uint8_t payload[], size_t payload_len)
 {
+  int error_count = 0;
+  for(int i=0; i<1; i++) {
   uint8_t data_sent_recieved[SPI_TRANSACTION_MAX_BYTES];
 
   int data_len = control_build_spi_data(data_sent_recieved, resid, cmd, payload, payload_len);
+  /*printf("resid 0x%x, cmd_id 0x%x, payload_len 0x%x\n",resid, cmd, payload_len);
+  for(int i=0; i<4; i++)
+  {
+	  printf("payload[%d] = 0x%x\n",i,payload[i]);
+  }*/
   bcm2835_spi_transfern((char *)data_sent_recieved, data_len);
 
+#if 1
+  memset(data_sent_recieved, 0, SPI_TRANSACTION_MAX_BYTES);
+  unsigned transaction_length = payload_len < 8 ? 8 : payload_len;  
+  
+  usleep(1000);
+  bcm2835_spi_transfern((char *)data_sent_recieved, payload_len);
+  printf("data_sent_recieved[0] = 0x%x, 0x%x, 0x%x, 0x%x\n",data_sent_recieved[0], data_sent_recieved[1], data_sent_recieved[2], data_sent_recieved[3]);
+#endif
+  }
+  //printf("error_count = %d\n", error_count);
   return CONTROL_SUCCESS;
 }
 
@@ -61,18 +78,30 @@ control_read_command(control_resid_t resid, control_cmd_t cmd,
   uint8_t data_sent_recieved[SPI_TRANSACTION_MAX_BYTES] = {0};
   int data_len = control_build_spi_data(data_sent_recieved, resid, cmd, payload, payload_len);
 
+  printf("control_read_command(): resid 0x%x, cmd_id 0x%x, payload_len 0x%x\n",resid, cmd, payload_len);
+  /*for(int i=0; i<4; i++)
+  {
+	  printf("payload[%d] = 0x%x\n",i,payload[i]);
+  }*/
+
   bcm2835_spi_transfern((char *)data_sent_recieved, data_len);
+
+  /*printf("Data recieved: ");
+  for(unsigned i=0; i<8; ++i) {
+    printf("%-3d ", (uint8_t) data_sent_recieved[i]);
+  }*/
   
-  usleep(delay_milliseconds * 1000);
+  usleep(1000);
   memset(data_sent_recieved, 0, SPI_TRANSACTION_MAX_BYTES);
   unsigned transaction_length = payload_len < 8 ? 8 : payload_len;  
 
-  bcm2835_spi_transfern((char *)data_sent_recieved, transaction_length);
-  DBG(printf("Data recieved: "));
+  bcm2835_spi_transfern((char *)data_sent_recieved, payload_len);
+  /*printf("Data recieved: ");
   for(unsigned i=0; i<transaction_length; ++i) {
-    DBG(printf("%-3d ", (uint8_t) data_sent_recieved[i]));
-  }
+    printf("%-3d ", (uint8_t) data_sent_recieved[i]);
+  }*/
 
+  printf("data_sent_recieved[0] = 0x%x, 0x%x, 0x%x, 0x%x\n",data_sent_recieved[0], data_sent_recieved[1], data_sent_recieved[2], data_sent_recieved[3]);
   memcpy(payload, data_sent_recieved, payload_len);
 
   return CONTROL_SUCCESS;
