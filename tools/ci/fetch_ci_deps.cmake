1df3d4b2a0abf79fbaca576b3941feda05d101e3
Jeremiah McCarthy <jeremiahmccarthy@xmos.com>
2023-01-19 10:21:52 -0500

    Update ci tests to run if deps core or io has changed

---
+++ tools/ci/fetch_ci_deps.cmake
@@ -0,0 +1,36 @@
+    ## Import dependencies
+    include(FetchContent)
+
+    IF(EXISTS ${CMAKE_BINARY_DIR}/dependencies/fwk_io)
+        message(STATUS "Skipped: dependencies/fwk_io")
+    else()
+        message(STATUS "Fetching: dependencies/fwk_io")
+        FetchContent_Declare(
+            fwk_io
+            GIT_REPOSITORY https://github.com/xmos/fwk_io.git
+            GIT_TAG        5576a36c7f85fd73f97b09e8a37b862e3f4771de
+            GIT_SHALLOW    TRUE
+            GIT_SUBMODULES_RECURSE TRUE
+            SOURCE_DIR     ${CMAKE_BINARY_DIR}/dependencies/fwk_io
+        )
+        FetchContent_Populate(fwk_io)
+    endif()
+
+    add_subdirectory(${CMAKE_BINARY_DIR}/dependencies/fwk_io)
+
+    IF(EXISTS ${CMAKE_BINARY_DIR}/dependencies/fwk_core)
+        message(STATUS "Skipped: dependencies/fwk_core")
+    else()
+        message(STATUS "Fetching: dependencies/fwk_core")
+        FetchContent_Declare(
+            fwk_core
+            GIT_REPOSITORY https://github.com/xmos/fwk_core.git
+            GIT_TAG        39018b4d2455a9f3ff2ade7717484bdd059407c4
+            GIT_SHALLOW    TRUE
+            GIT_SUBMODULES_RECURSE TRUE
+            SOURCE_DIR     ${CMAKE_BINARY_DIR}/dependencies/fwk_core
+        )
+        FetchContent_Populate(fwk_core)
+    endif()
+
+    add_subdirectory(${CMAKE_BINARY_DIR}/dependencies/fwk_core)
