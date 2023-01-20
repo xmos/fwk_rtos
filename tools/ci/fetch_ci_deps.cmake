586d255371a48ad7e5dc06710442f13c2565f210
Jeremiah McCarthy <jeremiahmccarthy@xmos.com>
2023-01-20 13:06:44 -0500

    Update to develop of core and io

--- tools/ci/fetch_ci_deps.cmake
+++ tools/ci/fetch_ci_deps.cmake
@@ -8,7 +8,7 @@
         FetchContent_Declare(
             fwk_io
             GIT_REPOSITORY https://github.com/xmos/fwk_io.git
-            GIT_TAG        76a08ae9b473f21aba7f0a1d839aad79f3edb064
+            GIT_TAG        74e28b3d254e8e1a3b949ac91cab599aecbb8c11
             GIT_SHALLOW    TRUE
             GIT_SUBMODULES_RECURSE TRUE
             SOURCE_DIR     ${CMAKE_BINARY_DIR}/dependencies/fwk_io
@@ -25,7 +25,7 @@
         FetchContent_Declare(
             fwk_core
             GIT_REPOSITORY https://github.com/xmos/fwk_core.git
-            GIT_TAG        d6a8050719621dd06dc2bab72c51af68853d851d
+            GIT_TAG        4cf4766513dcdc23113acfe29f534372dec42494
             GIT_SHALLOW    TRUE
             GIT_SUBMODULES_RECURSE TRUE
             SOURCE_DIR     ${CMAKE_BINARY_DIR}/dependencies/fwk_core
