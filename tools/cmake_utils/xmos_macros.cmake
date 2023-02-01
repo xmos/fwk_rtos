918e4b866c5095f13b4e44f9cde182552ba72714
Jon Carrier <jonathancarrier@xmos.com>
2023-02-01 14:02:06 -0500

    Updated create_filesystem_target CMake macro
    Added arguments to build filesystem using fatfs_mkimage.

--- tools/cmake_utils/xmos_macros.cmake
+++ tools/cmake_utils/xmos_macros.cmake
@@ -68,13 +68,39 @@ endmacro()
 
 ## Creates a filesystem file for a provided binary
 ##   filename must end in "_fat.fs"
-macro(create_filesystem_target _EXECUTABLE_TARGET_NAME)
-    add_custom_target(make_fs_${_EXECUTABLE_TARGET_NAME}
-      COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_LIST_DIR}/filesystem_support/${_EXECUTABLE_TARGET_NAME}_fat.fs ${_EXECUTABLE_TARGET_NAME}_fat.fs
-      DEPENDS ${_EXECUTABLE_TARGET_NAME}_fat.fs
+## Optional arguments can be used to specify other dependency targets, such as filesystem generators
+## create_filesystem_target(_EXECUTABLE_TARGET_NAME _FILESYSTEM_INPUT_DIR _IMAGE_SIZE _OPTIONAL_DEPENDS_TARGETS)
+macro(create_filesystem_target)
+  if(${ARGC} EQUAL 1)
+    add_custom_target(make_fs_${ARGV0} ALL
+      COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_LIST_DIR}/filesystem_support/${ARGV0}_fat.fs ${ARGV0}_fat.fs
+      DEPENDS ${ARGV0}_fat.fs
       COMMENT
         "Move filesystem"
+      VERBATIM
     )
+  elseif(${ARGC} EQUAL 3)
+    add_custom_target(make_fs_${ARGV0} ALL
+      COMMAND fatfs_mkimage --input=${ARGV1} --image_size=${ARGV2} --output=${ARGV0}_fat.fs
+      BYPRODUCTS
+        ${ARGV0}_fat.fs
+      COMMENT
+        "Create filesystem"
+      VERBATIM
+    )
+  elseif(${ARGC} EQUAL 4)
+    add_custom_target(make_fs_${ARGV0} ALL
+      COMMAND fatfs_mkimage --input=${ARGV1} --image_size=${ARGV2} --output=${ARGV0}_fat.fs
+      DEPENDS ${ARGV3}
+      BYPRODUCTS
+        ${ARGV0}_fat.fs
+      COMMENT
+        "Create filesystem"
+      VERBATIM
+    )
+  else()
+    message(FATAL_ERROR "Invalid number of arguments passed to create_filesystem_target")
+  endif()
 endmacro()
 
 ## Creates a directory populated with all components related to the data partition
