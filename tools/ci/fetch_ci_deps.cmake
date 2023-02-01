beb0446efa58c97ad0b7ae36b382067b3ee92c9a
Jon Carrier <jonathancarrier@xmos.com>
2023-02-01 14:01:14 -0500

    Resolved issue with fetching dependencies
    According to CMake docs:
    If 'GIT_SHALLOW' is enabled then 'GIT_TAG' works only with
    branch names and tags.  A commit hash is not allowed.

--- tools/ci/fetch_ci_deps.cmake
+++ tools/ci/fetch_ci_deps.cmake
@@ -9,7 +9,7 @@
             fwk_io
             GIT_REPOSITORY https://github.com/xmos/fwk_io.git
             GIT_TAG        74e28b3d254e8e1a3b949ac91cab599aecbb8c11
-            GIT_SHALLOW    TRUE
+            GIT_SHALLOW    FALSE
             GIT_SUBMODULES_RECURSE TRUE
             SOURCE_DIR     ${CMAKE_BINARY_DIR}/dependencies/fwk_io
         )
@@ -26,7 +26,7 @@
             fwk_core
             GIT_REPOSITORY https://github.com/xmos/fwk_core.git
             GIT_TAG        4cf4766513dcdc23113acfe29f534372dec42494
-            GIT_SHALLOW    TRUE
+            GIT_SHALLOW    FALSE
             GIT_SUBMODULES_RECURSE TRUE
             SOURCE_DIR     ${CMAKE_BINARY_DIR}/dependencies/fwk_core
         )
