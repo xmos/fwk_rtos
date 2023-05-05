cb3bb098f9f2f107203314a53794ab7db97edf77
Angel Cascarino <angelcascarino@xmos.com>
2023-05-05 16:51:07 +0100

    Uncommented some rtos_printfs

--- modules/drivers/usb/src/rtos_usb.c
+++ modules/drivers/usb/src/rtos_usb.c
@@ -35,7 +35,7 @@ static void usb_xud_thread(rtos_usb_t *ctx)
 
     (void) s_chan_in_byte(ctx->c_sof_xud);
 
-    //rtos_printf("Starting XUD_Main() on core %d with %d endpoints\n", rtos_core_id_get(), ctx->endpoint_count);
+    rtos_printf("Starting XUD_Main() on core %d with %d endpoints\n", rtos_core_id_get(), ctx->endpoint_count);
 
     XUD_Main(ctx->c_ep_out_xud,
              ctx->endpoint_count,
@@ -74,7 +74,7 @@ XUD_Result_t ep_transfer_complete(rtos_usb_t *ctx,
         res = xud_data_get_check(ctx->c_ep[ep_num][dir], len, is_setup);
 
         if (*len > ctx->ep_xfer_info[ep_num][dir].len) {
-            //rtos_printf("Length of %d bytes transferred on ep %d direction %d. Should have been <= %d\n", *len, ep_num, dir, ctx->ep_xfer_info[ep_num][dir].len);
+            rtos_printf("Length of %d bytes transferred on ep %d direction %d. Should have been <= %d\n", *len, ep_num, dir, ctx->ep_xfer_info[ep_num][dir].len);
         }
 
         xassert(*len <= ctx->ep_xfer_info[ep_num][dir].len);
@@ -84,16 +84,16 @@ XUD_Result_t ep_transfer_complete(rtos_usb_t *ctx,
             if (*is_setup) {
                 res = xud_setup_data_get_finish(ctx->ep[ep_num][dir]);
                 if (res == XUD_RES_ERR) {
-                    //rtos_printf("USB XFER ERROR from xud_setup_data_get_finish()!\n");
+                    rtos_printf("USB XFER ERROR from xud_setup_data_get_finish()!\n");
                 }
             } else {
                 res = xud_data_get_finish(ctx->ep[ep_num][dir]);
                 if (res == XUD_RES_ERR) {
-                    //rtos_printf("USB XFER ERROR from xud_data_get_finish()!\n");
+                    rtos_printf("USB XFER ERROR from xud_data_get_finish()!\n");
                 }
             }
         } else if (res == XUD_RES_ERR) {
-            //rtos_printf("USB XFER ERROR from xud_data_get_check()!\n");
+            rtos_printf("USB XFER ERROR from xud_data_get_check()!\n");
         }
     }
 
@@ -124,7 +124,7 @@ DEFINE_RTOS_INTERRUPT_CALLBACK(usb_isr, arg)
         }
     } else {
         ctx->ep[ep_num][dir] = XUD_InitEp(ctx->c_ep[ep_num][dir]);
-        //rtos_printf("EP %d %d initialized\n", ep_num, dir);
+        rtos_printf("EP %d %d initialized\n", ep_num, dir);
     }
 }
 #else /* USE_EP_PROXY */
