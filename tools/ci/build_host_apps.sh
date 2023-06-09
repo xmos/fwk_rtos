55b7c721e77353a9b3ddcdf1fd4ba03d314fa438
keithm-xmos <35048036+keithm-xmos@users.noreply.github.com>
2023-06-09 07:55:51 -0400

    fixed git dubious ownership error

--- tools/ci/build_host_apps.sh
+++ tools/ci/build_host_apps.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+if [ -f /.dockerenv ]; then
+    # Docker workaround for: "fatal: detected dubious ownership in repository"
+    git config --global --add safe.directory /fwk_rtos
+fi
+
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
 source ${REPO_ROOT}/tools/ci/helper_functions.sh
